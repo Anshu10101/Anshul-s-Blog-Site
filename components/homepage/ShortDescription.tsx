3b7318b25a0069297a6ac7eb4226d927de802576
Anshul Yadav <ay839990@gmail.com>
2025-04-29 14:19:35 +0530

    Initial commit

--- components/homepage/ShortDescription.tsx
+++ components/homepage/ShortDescription.tsx
@@ -1,8 +1,8 @@
 const ShortDescription = () => {
   return (
     <div className="mb-4 mt-4">
-      <p>I started learning to code in 2019 when I started college.</p>
-      <p>I landed my first job as a Back-end Developer in 2021.</p>
+      <p>I got interested in coding when I was in 9th standard.</p>
+      <p>I am currently pursuing MCA at SRM University KTR Chennai.</p>
       <p>I have a passion for JavaScript/Typescript and website development.</p>
       <p>I started this blog to practice my skill and share my knowledge.</p>
     </div>
